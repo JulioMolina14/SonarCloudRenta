c52eb44e3f8d7a7114fbb80507f37d98ff5eb2dc
JulioMolina14 <respaldo863@gmail.com>
2024-06-09 22:49:42 -0500

    final

--- Controllers/GestionAlquiler.js
+++ Controllers/GestionAlquiler.js
@@ -24,7 +24,7 @@ class ServicioAlquiler {
             const id_empleado = parseInt(Id_Empleados);
             const valorinicial = parseInt(Valor_Inicial);
             
-            const total = parseFloat(Total);
+            
 
 
 
@@ -98,7 +98,7 @@ class ServicioAlquiler {
             fecha.setDate(fecha.getDate() + 1);
 
             
-            const fechaFormateada = fecha.toISOString().slice(0, 19).replace("T", " ");
+            
 
             if ((Pago_Inicial != 0) || (Pago_Inicial == 0)) {
 
